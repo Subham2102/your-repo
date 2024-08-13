ae1994eda2ac06d91b6e76182d480531ca5cf95f
Subham2102 <shirko.nance@gmail.com>
2024-08-13 21:25:05 +0530

    update

---
+++ simple-ci-cd-app/test.js
@@ -0,0 +1,9 @@
+// test/test.js
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Sample Test', () => {
+  it('should return true', () => {
+    expect(true).to.be.true;
+  });
+});
