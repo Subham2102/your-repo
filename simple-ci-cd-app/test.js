942347827a0c75a7ad1f1b5794fda0e026383e8d
Subham2102 <shirko.nance@gmail.com>
2024-08-13 21:35:06 +0530

    deleted

--- simple-ci-cd-app/test.js
+++
@@ -1,9 +0,0 @@
-// test/test.js
-const chai = require('chai');
-const expect = chai.expect;
-
-describe('Sample Test', () => {
-  it('should return true', () => {
-    expect(true).to.be.true;
-  });
-});
